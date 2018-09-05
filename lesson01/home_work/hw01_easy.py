ae4489a23ebd420ea849411d7469646ef5152157
ineedmore <anvar.big@gmail.com>
2018-09-05 17:28:18 +0300

    Update hw01_easy.py

--- lesson01/home_work/hw01_easy.py
+++ lesson01/home_work/hw01_easy.py
@@ -18,7 +18,19 @@ __author__ = 'Ваши Ф.И.О.'
 # Не нужно решать задачу так:
 # print("a = ", b, "b = ", a) - это неправильное решение!
 
+a = input("Enter variable 1:")
+b = input("Enter variable 2:")
+c = (b,a)
+print(c)
+
+
 
 # Задача-3: Запросите у пользователя его возраст.
 # Если ему есть 18 лет, выведите: "Доступ разрешен",
 # иначе "Извините, пользование данным ресурсом только с 18 лет"
+
+age = int(input("What is your age?___"))
+if age >= 18:
+	print("Access allowed", type(age))
+else:	
+	print("Access denied", type(age))
